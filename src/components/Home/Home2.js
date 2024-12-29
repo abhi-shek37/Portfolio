093d504d273c9c4f1b6e66f26ad06eac4bbb3559
abhi-shek37 <ab.tiwari37@gmail.com>
2024-12-30 02:16:23 +0530

    issues rectified

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -74,7 +74,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href=""
+                  href="https://x.com/AbTiwari37"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
